5b2d50ff0dd322d5ae02e4460fa621ff8229e264
GitHub Action <action@github.com>
2021-05-25 05:38:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/heping0801/mvvm/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/heping0801/mvvm/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.heping0801.mvvm.listeners
 
+import com.github.heping0801.mvvm.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
